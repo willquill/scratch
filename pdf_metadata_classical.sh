91cc74ba2a64247dbf5abd093e5edde35c31c231
Will Coquillette <4646219+willquill@users.noreply.github.com>
2024-04-12 11:44:59 -0500

    Create pdf_metadata_classical.sh

---
+++ pdf_metadata_classical.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# forScore doc: http://forscore.co/developers-pdf-metadata/
+# Loop through each PDF file in the current directory and
+# replace Title and Author according to filename and
+# replace Subject with "Pop Rock"
+for pdf_file in *.pdf; do
+    
+    echo "Starting '$pdf_file'"
+    
+    # Extract the title and author from the filename using regex
+    # The regex assumes the format: Author - Title.pdf
+    # i.e. Bill Withers - Ain't No Sunshine.pdf
+    title=$(echo "$pdf_file" | grep -oP '(?<= - ).*(?=.pdf)')
+    author=$(echo "$pdf_file" | grep -oP '^[^-]+'| sed 's/ *$//g')
+    subject="Classical"
+    keywords="Classical"
+
+    # Use exiftool to set the title and author metadata
+    exiftool -Title="$title" -Author="$author" -Subject="$subject" -Keywords="$keywords" "$pdf_file"
+
+    echo "Set title '$title' and author '$author' and keywords '$keywords' for $pdf_file\n"
+
+    # Delete the original file created by exiftool
+    rm "${pdf_file}_original"
+done
